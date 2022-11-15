291eeb0400250fce971ae9bc995e9c46f919cb60
lindzr <munnsli@byui.edu>
2022-11-15 14:09:14 -0700

    called player2 in main program

--- Program.cs
+++ Program.cs
@@ -20,7 +20,8 @@ namespace Unit05
             // create the cast
             Cast cast = new Cast();
             cast.AddActor("food", new Food());
-            cast.AddActor("snake", new Snake());
+            cast.AddActor("playerOne", new Snake());
+            cast.AddActor("playerTwo", new Snake());
             cast.AddActor("score", new Score());
 
             // create the services
